cfcdf869d32ef34f421c9610fea7e44691e6515b
West Kraemer <kraemerwest@gmail.com>
2019-04-01 14:13:56 -0400

    add funcs and delegat and datasource

---
+++ emojiList/EmojiModel.swift
@@ -0,0 +1,9 @@
+//
+//  EmojiModel.swift
+//  emojiList
+//
+//  Created by West Kraemer on 4/1/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import Foundation
---
+++ emojiList/EmojiVC.swift
@@ -0,0 +1,29 @@
+//
+//  EmojiVC.swift
+//  emojiList
+//
+//  Created by West Kraemer on 4/1/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class EmojiVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+    
+
+}
