50d3b0315f2d9c988eaecbf821aa7a5a2ccfe250
West Kraemer <kraemerwest@gmail.com>
2019-04-01 14:04:37 -0400

    firs commit

---
+++ emojiList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  emojiList
+//
+//  Created by West Kraemer on 4/1/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
